ad30dae5324d79e74e3d4775fd055752caf73a2e
dnieln7 <dnieln7@gmail.com>
2021-08-13 19:22:17 -0500

    first commit

---
+++ app/src/main/java/com/dnieln7/fake17/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.dnieln7.fake17
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dnieln7.fake17.ui.AuthActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, AuthActivity::class.java))
+        finish()
+    }
+}
